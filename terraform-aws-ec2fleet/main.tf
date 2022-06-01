a08a26829af139dd7161e47fe1936048fc88f98c
mohammed.alnaggar <mohammed.alnaggar@vodafone.com>
2022-06-01 23:17:14 +0200

    inital

---
+++ terraform-aws-ec2fleet/main.tf
@@ -0,0 +1,21 @@
+## demonstrate for-each meta-argument
+
+resource "aws_instance" "servers" {
+    ami = "ami-0022f774911c1d690"
+    for_each = {
+        micro = "t2.micro"
+        nano = "t2.nano"
+    }
+
+    instance_type = each.value
+
+    tags = {
+      "Name" = "${each.key}-server"
+    }
+}
+
+output "pub-ips" {
+  value = values(aws_instance.servers2)[*].public_ip
+  # value = aws_instance.servers2["nano"].public_ip
+
+}
